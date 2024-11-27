33cb1569956660ece77c55067c9d373482d29ca6
ItishaBhatiya <itishabhatiya7@gmail.com>
2024-11-27 16:56:19 +0530

    mvc-crud

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dbconnect = require('./Config/db');
+const ProductRoute = require('./Routes/Product.Router');
+const app = express();
+
+app.use(express.json());
+require("dotenv").config();
+
+// app.get("/",(req,res)=>{
+//     res.send("welcome");
+// })
+
+app.use("/Products",ProductRoute)
+
+const PORT = process.env.PORT || 4050;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+    dbconnect();
+});
